606b49f04a39591c3eea8d9b45859eec8c23e443
krishnendu420 <52397631+krishnendu420@users.noreply.github.com>
2025-05-21 17:49:34 +0530

    Update bedrock_agent_runtime.py

--- amazon-bedrock-agent-test-ui-main/services/bedrock_agent_runtime.py
+++ amazon-bedrock-agent-test-ui-main/services/bedrock_agent_runtime.py
@@ -10,8 +10,8 @@ def invoke_agent(agent_id, agent_alias_id, session_id, prompt):
         client = boto3.session.Session().client(service_name="bedrock-agent-runtime", region_name='us-east-1')               #1 Add Region Name
         # See https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/bedrock-agent-runtime/client/invoke_agent.html
         response = client.invoke_agent(
-            agentId='EVLUORE74F',                     #2 Add Agent ID
-            agentAliasId='8T54ANHYGQ',                #3 Add Agent Alias ID
+            agentId='Y4JEWNVNFO',                     #2 Add Agent ID
+            agentAliasId='WVA85T7AQQ',                #3 Add Agent Alias ID
             enableTrace=True,
             sessionId=session_id,
             inputText=prompt
